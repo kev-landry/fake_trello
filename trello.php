d243ad63b199920d817118ab5cebe9aacc7b907a
kev-landry <kev.landry@free.fr>
2017-03-07 12:14:57 +0100

    travail css

--- trello.php
+++ trello.php
@@ -6,8 +6,18 @@ try {    //Connexion  BDD via PDO
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
-
     $reponse = $bdd->query('SELECT * FROM user WHERE pseudo = \''.$_SESSION['pseudo'].'\'');
+?>
+<div class="barre">
+</div>
+<div class="row-fluid">
+  <div class="col-lg-12">
+    <h1><medium>Bonjour <span id="bonjourUser"><?php echo $_SESSION['pseudo']; ?></span></medium></h1>
+</div>
+</div>
+
+<?php
+
     // On affiche chaque entrée une à une
     while ($donnees = $reponse->fetch()) {
         ?>
@@ -29,14 +39,14 @@ try {    //Connexion  BDD via PDO
                       $reponse->closeCursor();
     }
 ?>
-<div class="row-fluid">
+
+<div class="row-fluid"> <!--création liste -->
    <div class="col-lg-12 crealiste">
-    <form method="post" action="scriptBoard.php">
-        <button class="btn btn-primary" type="submit" name="liste">Créer liste</button>
+    <form method="post" action="crealiste.php">
+        <button id="boutonCrea"class="btn btn-primary" type="submit" name="liste">Créer liste</button>
         <input name="liste"/>
     </form>
   </div>
-
 </div>
 
 
@@ -49,13 +59,12 @@ try {    //Connexion  BDD via PDO
         <div class="col-lg-2 liste">
           <p><span id="titreListe"><?php echo $donnees['titre'];?></span></p>
           <div class="deleteliste">
-            <form method="post" action="suppliste.php">
+            <form method="post" action="suppliste.php">  <!--supprimer liste-->
                 <input type="hidden" class="btn btn-default btn-sm" name="suppliste" value="<?php echo $donnees['id'];?>"/>
                   <button type="submit" class="btn btn-default btn-sm">
                     <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
                   </button>
             </form>
-            <?php echo $donnees['id'];?>
           </div>
         </div>  <?php
       }
